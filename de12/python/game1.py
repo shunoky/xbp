0c8d8e78329cb8d9f4a75041a6ae1d714bd8ace0
shunoky <r202201998zk@jindai.jp>
2022-10-25 19:42:01 +0900

    20221025

---
+++ de12/python/game1.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+
+
+name=input("名前を教えて下さーい")
+print(name,"さんようこそ！")
+
+print("ゲームスタート")
---
+++ de12/python/work1.py
@@ -0,0 +1,15 @@
+name="ゴリラ"
+waist=100
+age=40
+hight=160
+speed=40
+
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
+# 学名はニシローゴリラ
+name="aaa"
+waist=84
+age=44
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
